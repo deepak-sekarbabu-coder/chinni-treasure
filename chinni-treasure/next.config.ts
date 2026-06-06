b2d30dae895435b88f33f3a85cd4c47dba8298e7
Deepak Sekarbabu <princedeepak2012@gmail.com>
2026-06-06 23:45:42 +0530

    Fixes

--- chinni-treasure/next.config.ts
+++ chinni-treasure/next.config.ts
@@ -21,7 +21,6 @@ const CORS_HEADERS = [
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
-  serverExternalPackages: ["pg", "@prisma/client", "@prisma/adapter-pg"],
   images: {
     remotePatterns: [
       {
